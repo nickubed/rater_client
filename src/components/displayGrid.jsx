03195e0d77b63674eda20e0c91db363f9b46490f
nickubed <nhpquandt@gmail.com>
2020-11-10 16:03:44 -0800

    This may be the ugliest way to render this table I could possibly manage, but its there. Needs refactor.

--- src/components/displayGrid.jsx
+++ src/components/displayGrid.jsx
@@ -1,10 +1,21 @@
 import React, {useState, useEffect} from 'react'
+// import '../static/griddle'
+import '../static/griddle.css'
 import {Redirect} from 'react-router-dom'
 
 const DisplayGrid = (props) => {
-    let [yourVillagers, setYourVillagers] = useState([])
     let [fGrid, setfGrid] = useState([])
+    let [dGrid, setdGrid] = useState([])
+    let [cGrid, setcGrid] = useState([])
+    let [bGrid, setbGrid] = useState([])
+    let [aGrid, setaGrid] = useState([])
+    let [sGrid, setsGrid] = useState([])
     let f = []
+    let d = []
+    let c = []
+    let b = []
+    let a = []
+    let s = []
     useEffect(() => {
         if(props.user){
             fetch(`${process.env.REACT_APP_DB_URL}/villager/${props.user.id}`)
@@ -14,6 +25,21 @@ const DisplayGrid = (props) => {
                         if(villager.usersVillagers.grade === 'F'){
                             f.push(villager)
                         }
+                        if(villager.usersVillagers.grade === 'D'){
+                            d.push(villager)
+                        }
+                        if(villager.usersVillagers.grade === 'C'){
+                            c.push(villager)
+                        }
+                        if(villager.usersVillagers.grade === 'B'){
+                            b.push(villager)
+                        }
+                        if(villager.usersVillagers.grade === 'A'){
+                            a.push(villager)
+                        }
+                        if(villager.usersVillagers.grade === 'S'){
+                            s.push(villager)
+                        }
                     })
                 }
             )
@@ -21,15 +47,34 @@ const DisplayGrid = (props) => {
     })
 
     const handleCheck = () => {
-        console.log(f)
         renderRow()
     }
 
     const renderRow = () => {
-        return f.map((villager, i) => {
+        setfGrid(f.map((villager, i) => {
             console.log('Chirp!')
-            return <td key={i}>{villager.name}</td>
-        })
+            return <td key={i}><img className="villagerPic" src={villager.img} alt={villager.name}></img></td>
+        }))
+        setdGrid(d.map((villager, i) => {
+            console.log('Chirp!')
+            return <td key={i}><img className="villagerPic" src={villager.img} alt={villager.name}></img></td>
+        }))
+        setcGrid(c.map((villager, i) => {
+            console.log('Chirp!')
+            return <td key={i}><img className="villagerPic" src={villager.img} alt={villager.name}></img></td>
+        }))
+        setbGrid(b.map((villager, i) => {
+            console.log('Chirp!')
+            return <td key={i}><img className="villagerPic" src={villager.img} alt={villager.name}></img></td>
+        }))
+        setaGrid(a.map((villager, i) => {
+            console.log('Chirp!')
+            return <td key={i}><img className="villagerPic" src={villager.img} alt={villager.name}></img></td>
+        }))
+        setsGrid(s.map((villager, i) => {
+            console.log('Chirp!')
+            return <td key={i}><img className="villagerPic" src={villager.img} alt={villager.name}></img></td>
+        }))
     }
     
     if(!props.user){
@@ -39,25 +84,32 @@ const DisplayGrid = (props) => {
     return(
         <div>
             <table>
-                <tr>
-                    <th>S</th>
-                    {f}
-                </tr>
-                <tr>
-                    <th>A</th>
-                </tr>
-                <tr>
-                    <th>B</th>
-                </tr>
-                <tr>
-                    <th>C</th>
-                </tr>
-                <tr>
-                    <th>D</th>
-                </tr>
-                <tr>
-                    <th>F</th>
-                </tr>
+                <tbody>
+                    <tr>
+                        <th>S</th>
+                        {sGrid}
+                    </tr>
+                    <tr>
+                        <th>A</th>
+                        {aGrid}
+                    </tr>
+                    <tr>
+                        <th>B</th>
+                        {bGrid}
+                    </tr>
+                    <tr>
+                        <th>C</th>
+                        {cGrid}
+                    </tr>
+                    <tr>
+                        <th>D</th>
+                        {dGrid}
+                    </tr>
+                    <tr>
+                        <th>F</th>
+                        {fGrid}
+                    </tr>
+                </tbody>
             </table>
             <button onClick={handleCheck}>Check</button>
         </div>
